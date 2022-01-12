46eebda6d9d1508e9196102c1a337b65c2907a9c
Kris Dekeyser <kris.dekeyser@libis.be>
2022-01-12 16:02:06 +0100

    Code cleanup

--- drop_index.sh
+++ drop_index.sh
@@ -4,4 +4,5 @@ source .env
 CORE="$1"
 [ "${CORE}" == "" ] && echo "Usage: ${0} <core name>" && exit
 
+echo "Unloading and deleting ${CORE} index ..."
 curl "${SOLR_HOST}/solr/admin/cores?action=UNLOAD&core=${CORE}&deleteIndex=true&deleteDataDir=true&deleteInstanceDir=true"
--- setup_index.sh
+++ setup_index.sh
@@ -4,13 +4,24 @@ source .env
 CORE="$1"
 [ "${CORE}" == "" ] && echo "Usage: ${0} <core name>" && exit
 
+DATA_FILE="$2"
+[[ -z "$DATA_FILE" ]] && echo "Data file $DATA_FILE does not exist" && exit
+
+echo "Creating ${CORE} index ..."
+
 docker exec -it ${SOLR_CONTAINER} mkdir -p ${SOLR_INDEX_DIR}/${CORE}/data
 docker exec -it ${SOLR_CONTAINER} cp -fr ./server/solr/configsets/_default/conf ${SOLR_INDEX_DIR}/${CORE}/conf
 
 curl "${SOLR_HOST}/solr/admin/cores?action=CREATE&name=${CORE}&instanceDir=${SOLR_INDEX_DIR}/${CORE}&dataDir=${SOLR_INDEX_DIR}/${CORE}/data&config=conf/solrconfig.xml"
 
-curl -X POST -H 'Content-type: application/json' -d @data/${CORE}/schema.json "${SOLR_HOST}/solr/${CORE}/schema"
+echo "Creating ${CORE} schema ..."
 
-ruby data/${CORE}/convert_data.rb aftap_lirias.csv
+curl -X POST -H 'Content-type: application/json' -d @${CORE}/schema.json "${SOLR_HOST}/solr/${CORE}/schema"
 
-curl -X POST -H 'Content-type: application/json' -d @data.json "${SOLR_HOST}/solr/${CORE}/update?commit=true"
+echo "Converting ${CORE} data from ${DATA_FILE} ..."
+
+ruby ${CORE}/convert_data.rb "$DATA_FILE" "${CORE}.json"
+
+echo "Indexing ${CORE} data ..."
+
+curl -X POST -H 'Content-type: application/json' -d "@${CORE}.json" "${SOLR_HOST}/solr/${CORE}/update?commit=true"
