b7aafb9a50297a3b206c4e06800f4a26be025951
Kris Dekeyser <kris@dva.be>
2022-01-18 00:34:29 +0100

    solrconfig add autocomplete request

--- setup_index.sh
+++ setup_index.sh
@@ -11,6 +11,7 @@ echo "Creating ${CORE} index ..."
 
 docker exec -it ${SOLR_CONTAINER} mkdir -p ${SOLR_INDEX_DIR}/${CORE}/data
 docker exec -it ${SOLR_CONTAINER} cp -fr ./server/solr/configsets/_default/conf ${SOLR_INDEX_DIR}/${CORE}/conf
+docker cp ${CORE}/solrconfig.xml ${SOLR_CONTAINER}:${SOLR_INDEX_DIR}/${CORE}/conf/solrconfig.xml
 
 R=$(curl -s "${SOLR_HOST}/solr/admin/cores?action=CREATE&name=${CORE}&instanceDir=${SOLR_INDEX_DIR}/${CORE}&dataDir=${SOLR_INDEX_DIR}/${CORE}/data&config=conf/solrconfig.xml")
 [ "$(echo "$R" | jq ".responseHeader.status")" = "0" ] || { echo "ERROR: $(echo "$R" | jq ".error.trace" | head -1)"; exit; }
