86176c000b6584a4bbb6e15af0bbccf58d14f6a0
Kris Dekeyser <kris.dekeyser@libis.be>
2022-03-02 09:10:38 +0100

    publisher lookup feature

--- server/config.ru
+++ server/config.ru
@@ -5,93 +5,185 @@ require 'json'
 require 'roda'
 require 'faraday'
 require 'httpx/adapters/faraday'
+require 'awesome_print'
 
 class App < Roda
+
   opts[:add_script_name] = 'covoc'
   plugin :json
   plugin :typecast_params
   plugin :public
 
+  NO_RESULT = { numFound: 0, start: 0, docs: [] }
+
   solr_host = ENV['SOLR_HOST'] || 'http://localhost:8983'
+  limo_host = ENV['LIMO_HOST'] || 'limo.libis.be'
+  log_level = ENV['LOG_LEVEL'] || 'error'
 
-  PUBLICATIONS = JSON.load(File.read(ENV['PUB_DATA'] || 'publications.json'))
-
-  conn = Faraday.new(solr_host, params: {indent: false}) do |f|
+  solr = Faraday.new(solr_host, params: {indent: false}) do |f|
     f.use Faraday::Request::UrlEncoded
     f.request :json
     f.response :json
+    f.response :logger, nil, {headers: true, bodies: false, log_level: log_level}
     f.adapter :httpx
-    f.response :logger if ENV['DEBUG_LOG']
+  end
+
+  limo = Faraday.new('https://services.libis.be', params: {host: limo_host, institution: 'lirias', sort: 'scdate'}) do |f|
+    f.use Faraday::Request::UrlEncoded
+    f.request :json
+    f.response :json
+    f.response :logger, nil, {headers: true, bodies: false, log_level: log_level}
+    f.adapter :httpx
   end
 
   route do |r|
 
+    # Static content (covoc.js)
     r.public
 
+    # Entrypoint for Authors search
     r.get 'authors' do
 
+      min_from = 0
+      min_page = 1
+      default_from = 0
+      default_page = 10
+
+      # Get parameters
       str = typecast_params.str('q')
       next unless str
 
-      from = typecast_params.int('from')
-      per_page = typecast_params.int('per_page')
-      per_page ||= 10
+      from = [min_from, typecast_params.int('from') || default_from].max
+      per_page = [min_page, typecast_params.int('per_page') || default_page].max
 
+      # Perform Solr search
+      p = {
+        start: from,
+        rows: per_page,
+        q: str
+      }
       res = case str
       when /^(\b(u?\d*))+$/
-        conn.get('/solr/authors/number') do |req|
-          req.params['rows'] = per_page
-          req.params['q'] = str
-          req.params['start'] = from if from && from >= 0
+        solr.get('/solr/authors/number') do |req|
+          req.params.merge!(p)
         end
       when /^(\b\w*(\.|@)\w*)+$/
-        conn.get('/solr/authors/email') do |req|
-          req.params['rows'] = per_page
-          req.params['q'] = str
-          req.params['start'] = from if from && from >= 0
+        solr.get('/solr/authors/email') do |req|
+          req.params.merge!(p)
         end
       when /^(\b[a-zA-z]+,?)+$/
-        conn.get('/solr/authors/name') do |req|
-          req.params['rows'] = per_page
-          req.params['q'] = str
-          req.params['start'] = from if from && from >= 0
+        solr.get('/solr/authors/name') do |req|
+          req.params.merge!(p)
         end
       else
-        conn.get('/solr/authors/any') do |req|
-          req.params['rows'] = per_page
-          req.params['q'] = str
-          req.params['start'] = from if from && from >= 0
+        solr.get('/solr/authors/any') do |req|
+          req.params.merge!(p)
         end
       end
-      res = JSON.parse(res.body)['response']
+
+      # Check response error
+      unless res&.success?
+        Logger($stdout).new.error("Solr response [#{res&.status}] #{res&.headers}")
+        return NO_RESULT
+      end
+
+      # Parse JSON result 
+      res = JSON.parse(res.body)&.dig('response')
+      return NO_RESULT unless res
+
+      # Set start, next and previous values
       next_start = res['start'] + per_page
-      prev_start = res['start'] - per_page
-      prev_start = 0 if prev_start < 0
-      res['next'] = next_start if next_start < res['numFound']
-      res['prev'] = prev_start if res['start'] != 0
+      prev_start = [min_from, res['start'] - per_page].max
+      res['next'] = next_start if next_start < res['numFound'] + min_from
+      res['prev'] = prev_start if res['start'] != min_from
+
+      # Finally, return the response
       res
+
     end
 
+    # Entrypoint for Publications search
     r.get 'publications' do
 
+      min_from = 1
+      min_page = 1
+      default_from = 1
+      default_page = 1
+
+      # Get parameters
       str = typecast_params.str('q')
       next unless str
 
-      from = typecast_params.int('from') || 0
-      per_page = typecast_params.int('per_page') || 10
+      from = [min_from, typecast_params.int('from') || default_from].max
+      per_page = [min_page, typecast_params.int('per_page') || default_page].max
 
+      # Perform Limo search
+      p = {
+        from: from,
+        step: per_page
+      }
+      res = case str
+      when /^\d+$/
+        limo.get('/search') do |req|
+          req.params.merge!(p)[:query] = "any:LIRIAS#{str}"
+        end
+      when /^u\d+$/
+        limo.get('/search') do |req|
+          req.params.merge!(p)[:query] = "user:#{str}"
+        end
+      when /^@/
+        limo.get('/search') do |req|
+          req.params.merge!(p)[:query] = "author:#{str[1..-1]}"
+        end
+      else
+        limo.get('/search') do |req|
+          req.params.merge!(p)[:query] = "any:#{str}"
+        end
+      end
+
+      # Check response error
+      unless res.success?
+        Logger($stdout).new.error("Limo response [#{res.status}] #{res.headers}")
+        return NO_RESULT
+      end
+
+      # Build the response data
+      res = res.body
       res = {
-        'numFound' => PUBLICATIONS.size,
-        'start' => from,
-        'numFoundExact' => true,
-        'docs' => PUBLICATIONS
+        'numFound' => res['count'],
+        'start' => res['from'],
+        'docs' => res['data'].map do |data|
+          # Can't trust output to be single value
+          creator = [data.dig('display','creator')].flatten.first
+          title = [data.dig('display','title')].flatten.first
+          ispartof = [data.dig('display','ispartof')].flatten.first
+          url = [data.dig('links', 'backlink')].flatten.first
+          doi = nil
+          issn = nil
+          [data.dig('display','identifier')].flatten.each do |identifier|
+            next unless identifier
+            doi ||= identifier.scan(/\$\$CDOI:\$\$V([^$]*)/)&.first&.first
+            issn ||= identifier.scan(/\$\$CISSN:\$\$V([^$]*)/)&.first&.first
+           end
+          {
+            id: data.dig('id')&.gsub(/^LIRIAS/i, ''),
+            title: title,
+            citation: "#{creator}. &quot;#{title}.&quot; #{ispartof}",
+            url: url
+          }.tap do |x|
+            x[:doi] = doi if doi
+            x[:issn] = issn if issn
+          end
+        end
       }
 
+      # Set start, next and previous values
       next_start = res['start'] + per_page
-      prev_start = res['start'] - per_page
-      prev_start = 0 if prev_start < 0
-      res['next'] = next_start if next_start < res['numFound']
-      res['prev'] = prev_start if res['start'] != 0
+      prev_start = [min_from, res['start'] - per_page].max
+      res['next'] = next_start if next_start < res['numFound'] + min_from
+      res['prev'] = prev_start if res['start'] != min_from
+
+      # Finally, return the response
       res
 
     end
--- server/public/js/covoc.js
+++ server/public/js/covoc.js
@@ -25,7 +25,7 @@ var personModalHelptext =
   "Type at least 3 characters per word for a meaningful search and '-' and leading 0's can be omitted. " + 
   "Include a '@' to search for email addresses.";
 var personSearchBoxId = 'person-search-box';
-var personSearchResultsID = 'person-search-results';
+var personSearchResultsId = 'person-search-results';
 var personElementIdAttribute = 'data-person-element-id';
 var personSearchIconText = 'Search in KU Leuven';
 
@@ -35,21 +35,41 @@ var authorSelector = "#metadata_author ~ .dataset-field-values .edit-compound-fi
 // Selector for all the contact compound fields
 var contactSelector = "#metadata_datasetContact ~ .dataset-field-values .edit-compound-field";
 
+var pubModalId = 'publication-modal';
+var pubModalTitle = 'Search for publication in Lirias';
+var pubModalPlaceholder = 'Search for publication';
+var pubModalHelptext = 'Type search term and hit the Enter key to search.';
+var pubSearchBoxId = 'publication-search-box';
+var pubSearchResultsId = 'publication-search-results';
+var pubElementIdAttribute = 'data-publication-element-id';
+var pubSearchIconText = 'Search in Lirias';
+
+//Selector for all the publication compound fields
+var publicationSelector = "#metadata_publication ~ .dataset-field-values .edit-compound-field";
+
 /* The browser will run this code the first time the editor is opened and each time a multiple field instance is 
  * added or removed. This code is reposible for creating the HTML for the dialog box, adding a search button to 
  * the name fields and creating the triggers for initializing the dialog box and the search action itself.
  */
 $(document).ready(function() {
 
-  // Create modal
+  // Create person search modal
   createModal(personModalId, personModalTitle, personModalPlaceholder, personModalHelptext, 
-    personSearchBoxId, personSearchResultsID, personElementIdAttribute, personQuery);
+    personSearchBoxId, personSearchResultsId, personElementIdAttribute, personQuery);
 
   // Put a search button after each name field
-  putNameSearchIcon(authorSelector, personElementIdAttribute, personModalId, personSearchIconText);
+  putSearchIcon(authorSelector, 0, personElementIdAttribute, personModalId, personSearchIconText);
 
   // Put a search button after each contact name field
-  putNameSearchIcon(contactSelector, personElementIdAttribute, personModalId, personSearchIconText);
+  putSearchIcon(contactSelector, 0, personElementIdAttribute, personModalId, personSearchIconText);
+
+  // Create publication search modal
+  createModal(pubModalId, pubModalTitle, pubModalPlaceholder, pubModalHelptext,
+    pubSearchBoxId, pubSearchResultsId, pubElementIdAttribute, publicationQuery);
+
+  // Put search button after each Lirias ID field
+  putSearchIcon(publicationSelector, 3, pubElementIdAttribute, pubModalId, pubSearchIconText);
+
 });
 
 // Generic function to create a serch box
@@ -117,22 +137,22 @@ function createModal(id, title, placeholder, helptext, searchBoxId, resultsId, e
 }
 
 // Generic function that add a search button to the name input fields
-function putNameSearchIcon(selector, elementIdAttribute, modalId, searchIconText) {
+function putSearchIcon(selector, childNr, elementIdAttribute, modalId, searchIconText) {
   // Iterate over compund elements
   document.querySelectorAll(selector).forEach(element => {
     // 'search_added' class marks elements that have already been processed
     if (!element.classList.contains('search_added')) {
       element.classList.add('search_added');
-      // First child is element that encapsulates label and input of name
-      let nameField = element.children[0];
+      // Select child is element that the search box needs to be attachted to
+      let metadataField = element.children[childNr];
       // Input field within
-      let nameInput = nameField.querySelector('input');
+      let inputField = metadataField.querySelector('input');
       // We create an bootstrap input group ...
       let wrapper = document.createElement('div');
       wrapper.className = 'input-group';
       wrapper.style.display = 'flex';
       // ... containing the input field ...
-      wrapper.appendChild(nameInput);
+      wrapper.appendChild(inputField);
       // ... and a new seach button ...
       let searchButton = document.createElement('button');
       element.setAttribute('aria-describedby', searchButton.id);
@@ -141,15 +161,99 @@ function putNameSearchIcon(selector, elementIdAttribute, modalId, searchIconText
       searchButton.setAttribute('title', searchIconText);
       searchButton.setAttribute('data-toggle', 'modal');
       searchButton.setAttribute('data-target', '#' + modalId);
-      searchButton.setAttribute(elementIdAttribute, nameInput.id);
+      searchButton.setAttribute(elementIdAttribute, inputField.id);
       let searchIcon = document.createElement('span');
       searchIcon.className = 'glyphicon glyphicon-search no-text';
       searchButton.appendChild(searchIcon);
       wrapper.appendChild(searchButton);
       // ... and add that to the encapsulating element.
-      nameField.appendChild(wrapper);
+      metadataField.appendChild(wrapper);
     }
   })
+}
+
+var page_size = 10; // Number of results that will be displayed on a single page
+
+// Lauches a query to the external vocabulary server and fills in the results in the table element of the dialog searchBox
+
+/* arguments:
+ *  - str (String): text to search for
+ *  - start (Integer): start position of the results (for paginated results)
+ * the result of the query is a JSON object with at least:
+ *  - numFound (Integer): total number of results
+ *  - start (Integer): the start position of the current resultset
+ *  - prev (Integer - optional): the start position for the previous page, not present for first page
+ *  - next (Integer - optional): the start position for the next page, not present for last page
+ *  - docs (Array of JSON objects): list of results with:
+ *    - uNumber (String): user identification number
+ *    - fullName (String): user name in '<lastname>, <firstname>' format
+ *    - affiliation (String): user's affiliation organization
+ *    - eMail (String): user's email address
+ *    - orcid (String - optional): user's OCRID number
+ * the query server is expected to accept these query parameters:
+ *  - q: the search term
+ *  - from: the starting position for the results
+ *  - per_page: the number of results to return per page
+ */
+
+function personQuery(str, start) {
+  if (!str) {
+    return;
+  }
+  if (!start) {
+    start = 0;
+  }
+  // Vocabulary search REST call
+  fetch("/covoc/authors?q=" + str + '&from=' + start + '&per_page=' + page_size)
+  .then(response => response.json())
+  .then(data => {
+    let table = document.querySelector('#' +  personSearchResultsId + ' tbody');
+    // Clear table content
+    table.innerHTML = ''
+    // Add pagination header
+    if (data.hasOwnProperty('prev') || data.hasOwnProperty('next')) {
+      table.innerHTML += 
+      '<tr>' + 
+        '<td class="row" colspan="4">' +
+          '<div class="col-sm-2">' +
+            ((data.hasOwnProperty('prev'))
+              ? '<span class="btn btn-default btn-xs pull-left" accesskey="p" onclick="personQuery(\'' + str + '\', ' + data.prev + ')">&lt;&lt;</span>'
+              : ''
+            ) +
+          '</div>' +
+          '<div class="col-sm-8 text-center">' +
+            (start + 1) + '-' + (start + data.docs.length) + ' of ' + data.numFound +
+          '</div>' +
+          '<div class="col-sm-2">' +
+            ((data.hasOwnProperty('next'))
+              ? '<span class="btn btn-default btn-xs pull-right" accesskey="n" onclick="personQuery(\'' + str + '\', ' + data.next + ')">&gt;&gt;</span>'
+              : ''
+            ) +
+          '</div>' +
+        '</td>' +
+      '</tr>';
+    }
+    // Iterate over results
+    data.docs.forEach((doc) => {
+      // Get ID of target input element
+      let id = document.getElementById(personSearchBoxId).getAttribute(personElementIdAttribute);
+      // Add a table row for the doc
+      table.innerHTML += 
+      '<tr title="' + doc.eMail + '">' +
+        '<td>' + doc.fullName + '</td>' +
+        '<td><a href="https://www.kuleuven.be/wieiswie/nl/person/' + doc.uNumber.slice(1) + '" target="_blank">' + doc.uNumber + '</a></td>' +
+        '<td>' + 
+          ((doc.orcid) ? '<a href="https://orcid.org/' + doc.orcid + '" target="_blank">' + doc.orcid + '</a>' : '') + 
+        '</td>' + 
+        '<td>' + 
+          '<span ' + 
+            'class="btn btn-default btn-xs glyphicon glyphicon-import pull-right" title="import" ' + 
+            'onclick="importPersonData(\'' + id + '\', \'' + doc.fullName + '\', \'' + doc.eMail + '\', \'' + doc.affiliation + '\', \'' + (doc.orcid || '') + '\');">' + 
+          '</span>' + 
+        '</td>' + 
+      '</tr>';
+    });
+  });
 }
 
 // Import the query result data into the metadata form
@@ -205,8 +309,6 @@ function importPersonData(id, fullName, emailAddress, affiliation, orcid) {
   $('#' + personModalId).modal('hide');
 }
 
-var page_size = 10; // Number of results that will be displayed on a single page
-
 // Lauches a query to the external vocabulary server and fills in the results in the table element of the dialog searchBox
 
 /* arguments:
@@ -218,36 +320,40 @@ var page_size = 10; // Number of results that will be displayed on a single page
  *  - prev (Integer - optional): the start position for the previous page, not present for first page
  *  - next (Integer - optional): the start position for the next page, not present for last page
  *  - docs (Array of JSON objects): list of results with:
- *    - uNumber (String): user identification number
- *    - fullName (String): user name in '<lastname>, <firstname>' format
- *    - affiliation (String): user's affiliation organization
- *    - eMail (String): user's email address
- *    - orcid (String - optional): user's OCRID number
+ *    - id (String): publication source id (Lirias number)
+ *    - doi (String): the doi of the publication, if present
+ *    - issn (String): the issn of the serial, if present
+ *    - url (String): publication URL
+ *    - title (String): publication title
+ *    - citation (String): citation text
  * the query server is expected to accept these query parameters:
  *  - q: the search term
  *  - from: the starting position for the results
  *  - per_page: the number of results to return per page
  */
 
-function personQuery(str, start) {
+function publicationQuery(str, start) {
+  if (!str) {
+    return;
+  }
   if (!start) {
     start = 0;
   }
   // Vocabulary search REST call
-  fetch("/covoc/authors?q=" + str + '&from=' + start + '&per_page=' + page_size)
+  fetch("/covoc/publications?q=" + str + '&from=' + start + '&per_page=' + page_size)
   .then(response => response.json())
   .then(data => {
-    let table = document.querySelector('#' +  personSearchResultsID + ' tbody');
+    let table = document.querySelector('#' +  pubSearchResultsId + ' tbody');
     // Clear table content
     table.innerHTML = ''
     // Add pagination header
     if (data.hasOwnProperty('prev') || data.hasOwnProperty('next')) {
       table.innerHTML += 
       '<tr>' + 
-        '<td class="row" colspan="4">' +
+        '<td class="row" colspan="2">' +
           '<div class="col-sm-2">' +
             ((data.hasOwnProperty('prev'))
-              ? '<span class="btn btn-default btn-xs pull-left" accesskey="p" onclick="personQuery(\'' + str + '\', ' + data.prev + ')">&lt;&lt;</span>'
+              ? '<span class="btn btn-default btn-xs pull-left" accesskey="p" onclick="publicationQuery(\'' + str + '\', ' + data.prev + ')">&lt;&lt;</span>'
               : ''
             ) +
           '</div>' +
@@ -256,7 +362,7 @@ function personQuery(str, start) {
           '</div>' +
           '<div class="col-sm-2">' +
             ((data.hasOwnProperty('next'))
-              ? '<span class="btn btn-default btn-xs pull-right" accesskey="n" onclick="personQuery(\'' + str + '\', ' + data.next + ')">&gt;&gt;</span>'
+              ? '<span class="btn btn-default btn-xs pull-right" accesskey="n" onclick="publicationQuery(\'' + str + '\', ' + data.next + ')">&gt;&gt;</span>'
               : ''
             ) +
           '</div>' +
@@ -266,22 +372,73 @@ function personQuery(str, start) {
     // Iterate over results
     data.docs.forEach((doc) => {
       // Get ID of target input element
-      let id = document.getElementById(personSearchBoxId).getAttribute(personElementIdAttribute);
+      let id = document.getElementById(pubSearchBoxId).getAttribute(pubElementIdAttribute);
       // Add a table row for the doc
       table.innerHTML += 
-      '<tr title="' + doc.eMail + '">' +
-        '<td>' + doc.fullName + '</td>' +
-        '<td><a href="https://www.kuleuven.be/wieiswie/nl/person/' + doc.uNumber.slice(1) + '" target="_blank">' + doc.uNumber + '</a></td>' +
-        '<td>' + 
-          ((doc.orcid) ? '<a href="https://orcid.org/' + doc.orcid + '" target="_blank">' + doc.orcid + '</a>' : '') + 
-        '</td>' + 
+      '<tr>' +
+        '<td><a href="' + doc.url + '" target="_blank">' + doc.title + '</a></td>' +
         '<td>' + 
           '<span ' + 
             'class="btn btn-default btn-xs glyphicon glyphicon-import pull-right" title="import" ' + 
-            'onclick="importPersonData(\'' + id + '\', \'' + doc.fullName + '\', \'' + doc.eMail + '\', \'' + doc.affiliation + '\', \'' + (doc.orcid || '') + '\');">' + 
+            'onclick="importPublicationData(\'' + id + '\', \'' + doc.citation + '\', \'' + doc.id + '\', \'' + (doc.doi || '') + '\', \'' + (doc.issn || '') + '\', \'' + (doc.url || '') + '\');">' + 
           '</span>' + 
         '</td>' + 
       '</tr>';
     });
   });
+}
+
+// Import the query result data into the metadata form
+// arguments:
+// - id (String): identifier of the related metadata input field
+// - citation, sourceId, doi, issn, url: publication data
+function importPublicationData(id, citation, sourceId, doi, issn, url) {
+  // Get the search input field
+  let idInput = document.getElementById(id);
+  // Up to the compound element
+  let compoundElement = idInput.closest('.search_added');
+  // 1st child contains the citation
+  let citationInput = compoundElement.children[0].querySelector('textarea');
+  // 5th child contains the URL
+  let urlInput = compoundElement.children[4].querySelector('input');
+  // Fill-in name and affiliation
+  idInput.value = sourceId;
+  citationInput.value = citation;
+  urlInput.value = url;
+
+  // 2nd child is the identifier scheme wrapper and contains multiple elements:
+  // - a label element that shows the current selected value
+  let identifierSchemeText = compoundElement.children[1].querySelector('.ui-selectonemenu-label');
+  // - a select element that contains the drop-down
+  let identifierSchemeSelect = compoundElement.children[1].querySelector('select');
+  // 3rd child contains the input element for the identifier
+  let identifierInput = compoundElement.children[2].querySelector('input');
+  // Fill-in doi
+  if (doi) {
+    identifierInput.value = doi;
+    // Setting the dropdown box is trickier:
+    // First get the option from the select list whose text content matches the value you want to set
+    let option = Array.from(identifierSchemeSelect.querySelectorAll('option')).find(el => el.text === 'doi');
+    // Then get the value from that option and set the select element's value with it
+    identifierSchemeSelect.value = option.getAttribute('value');
+    // But you should also set the label field or your selection will not display
+    identifierSchemeText.textContent = 'doi';
+  } else if(issn) {
+    identifierInput.value = issn;
+    // Setting the dropdown box is trickier:
+    // First get the option from the select list whose text content matches the value you want to set
+    let option = Array.from(identifierSchemeSelect.querySelectorAll('option')).find(el => el.text === 'issn');
+    // Then get the value from that option and set the select element's value with it
+    identifierSchemeSelect.value = option.getAttribute('value');
+    // But you should also set the label field or your selection will not display
+    identifierSchemeText.textContent = 'issn';
+  } else {
+    // clear the orcid input box and dropdown box
+    identifierInput.value = '';
+    // Default text is in the first option
+    identifierSchemeText.textContent = identifierSchemeSelect.children[0].text;
+    identifierSchemeSelect.value = '';
+  }
+  // Close the dialog box when the import is done
+  $('#' + pubModalId).modal('hide');
 }
